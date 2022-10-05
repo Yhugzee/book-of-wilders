82926eb393751f4b3ca21a90d3c85d029d05c010
Charlotte Kieffer <kindred242@gmail.Com>
2022-10-05 11:16:37 +0200

    fixed a typo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const wilders = [
     {
         name: "Adnan Bakaev",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "J'aime presque tous les genres musicaux",
         hobbie: "Livres, films et football.",
         likewcs: "J'aime le monde de la technologie, la programmation, apprendre quelque chose de nouveau, et aussi un grand désir d'avoir un travail que j'aime.",
@@ -10,7 +10,7 @@ const wilders = [
     {
         name: "Alexis Durin",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "Rap et funk",
         hobbie: "Sports de combat",
         likewcs: "Je souhaitais me reconvertir et une formation rapide me convient",
@@ -18,7 +18,7 @@ const wilders = [
     {
         name: "Benoît Galey",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "Variété/Reggae/Rock/Rap ",
         hobbie: "Randonnée, ski",
         likewcs: "J'aime le code",
@@ -26,7 +26,7 @@ const wilders = [
     {
         name: "Charlotte Kieffer",
         image: "./profiles/charlotte.png",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "Le rock et le métal (les Foo Fighters, Radiohead, Ministry, The Black Keys)",
         hobbie: "La photo, les jeux vidéos, la science-fiction",
         likewcs: "L'envie de découvrir un nouveau métier avec de nouveaux défis !"
@@ -35,7 +35,7 @@ const wilders = [
     {
         name: "Edouard Tractere",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "Rape, classique et funk",
         hobbie: "Les jeux vidéos",
         likewcs: "Je suis là pour approfondir mes connaissances et j'aime la méthode de travail de la Wild",
@@ -43,7 +43,7 @@ const wilders = [
     {
         name: "Farid Himeur",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "",
         hobbie: "",
         likewcs: "",
@@ -51,7 +51,7 @@ const wilders = [
     {
         name: "Frédéric Michel",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux",
+        qmusic: "Tes goûts musicaux",
         music: "Je n'ai pas de style préféré, je m'interresse à tout.",
         hobbie: "Informatique - Arts marrtiaux (karaté) - échecs -photo - secourisme",
         likewcs: "Le bus, non plus sérieusement, une reconversion professionnelle, une attirance certaine pour le code et un grand intérêt pour l'apprentissage permanent.",
@@ -59,7 +59,7 @@ const wilders = [
     {
         name: "Herimanana Rasolonirina",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux",
+        qmusic: "Tes goûts musicaux",
         music: "La musique Malgache",
         hobbie: "Pas de hobbies particuliers",
         likewcs: "Je suis passionné par le code depusi longtemps",
@@ -67,7 +67,7 @@ const wilders = [
     {
         name: "Hugo Rodriguez",
         image: "./profiles/hugo.png",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "Lofi & Rap (Lil Peep ❤️)",
         hobbie: "L'informatique et les jeux vidéos",
         likewcs: "Le plaisir de coder",
@@ -75,7 +75,7 @@ const wilders = [
     {
         name: "Jessy Hazart",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "Peu importe tant que j'aime ce que j'écoute.",
         hobbie: "Programmation, jeux vidéo, football",
         likewcs: "J'aime coder"
@@ -83,7 +83,7 @@ const wilders = [
     {
         name: "José Alves",
         image: "./profiles/jose.png",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "Funk et classique",
         hobbie: "Le piano, les arts martiaux et la psychologie",
         likewcs: "Je voulaois me reconvertir dans un métier d'avenir",
@@ -91,7 +91,7 @@ const wilders = [
     {
         name: "Louen Maginot",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "Tout type de musique",
         hobbie: "Le sport et l'informatique",
         likewcs: "Je souhaite créer des site web et en faire mon métier",
@@ -99,7 +99,7 @@ const wilders = [
     {
         name: "Lucie Froissart",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "J'aime tous les styles",
         hobbie: "Les jeux vidéos, la lecture, les animés",
         likewcs: "Je veux apprendre à coder grâce à une formation courte",
@@ -107,7 +107,7 @@ const wilders = [
     {
         name: "Mounir Mekoui",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "Le rap français et le hip-hop",
         hobbie: "Les jeux vidéos et sortir avec mes amis",
         likewcs: "Pour construire un avenir autour de mon appétance pour le numérique",
@@ -115,14 +115,14 @@ const wilders = [
     {
         name: "Olivier Lopez",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "Rock - Electro",
         hobbie: "Jeux vidéo - Série TV / Anime - Voyage - Moto"
     },
     {
         name: "Olivier Nou",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "Le métal",
         hobbie: "La musique et la photo",
         likewcs: "Pour apprendre le code et le transmettre à ma fille",
@@ -130,7 +130,7 @@ const wilders = [
     {
         name: "Pierre Yves Gille Mignon",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "Le rap et le hardcore",
         hobbie: "Le sport et les jeux vidéos",
         likewcs: "Le système d'apprentissage court et intensif me plait",
@@ -138,7 +138,7 @@ const wilders = [
     {
         name: "Robin Kolasinski",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "Celtic métal",
         hobbie: "Jeux vidéos, bière",
         likewcs: "Je cherchais une formation en développement web",
@@ -146,7 +146,7 @@ const wilders = [
     {
         name: "Rohan Molinillo",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "Métal, dubstep et pop",
         hobbie: "Jeux vidéos, musique et création photoshop",
         likewcs: "En reconversion professionnelle, j'ai souhaité me diriger vers le métier de développeur web.",
@@ -154,7 +154,7 @@ const wilders = [
     {
         name: "Romain Bronquard",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "Le rap",
         hobbie: "Les jeux vidéos et les animés",
         likewcs: "Cela fait 3 ans que je souhaite devenir developpeur web",
@@ -162,7 +162,7 @@ const wilders = [
     {
         name: "Rémi Lagreze",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "Rap et rock",
         hobbie: "Les jeux vidéos",
         likewcs: "L'amour du code",
@@ -170,7 +170,7 @@ const wilders = [
     {
         name: "Simon Bullado",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "Le métal et le rock",
         hobbie: "Le VTT et les PC",
         likewcs: "Je veux me reconvertir etd evenir développeur web",
@@ -178,7 +178,7 @@ const wilders = [
     {
         name: "Valentin Marlois",
         image: "./images/blank-profile.jpeg",
-        qmusic: "Tes gouts musicaux :",
+        qmusic: "Tes goûts musicaux :",
         music: "Rap et electro",
         hobbie: "La moto, la pêche",
         likewcs: "Je cherchais une formation accessible à tous et de courte durée",
