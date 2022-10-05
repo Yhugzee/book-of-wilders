0f5169162278bfc68f326bcc9159e19cd75afa5a
FMichel5173 <fmichel81@sfr.fr>
2022-10-05 17:21:18 +0200

    Modify pictures link oy Farid

--- index.js
+++ index.js
@@ -323,16 +323,16 @@ for (let i = 0; i < wilders.length; i++) {
 let opened = false
 
 function borderChange() {
-    
-    if(opened === false){
-    document.getElementById("wild-section").style.borderBottomLeftRadius = "0rem";
-      document.getElementById("wild-section").style.borderBottomRightRadius = "0rem";
-   opened = true
-    }else {   
-        setTimeout (() => {
+
+    if (opened === false) {
+        document.getElementById("wild-section").style.borderBottomLeftRadius = "0rem";
+        document.getElementById("wild-section").style.borderBottomRightRadius = "0rem";
+        opened = true
+    } else {
+        setTimeout(() => {
             document.getElementById("wild-section").style.borderBottomLeftRadius = "2rem";
             document.getElementById("wild-section").style.borderBottomRightRadius = "2rem";
-        },335)
-      opened = false;
-  }
+        }, 335)
+        opened = false;
+    }
 }
